c015e8302e171cbe878f8b27b712650a567d1ab0
Jason <j_ash@salemstate.edu>
2023-04-04 11:00:49 -0400

    trivial change for ferry schedule

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -31,6 +31,8 @@ export default function Navbar() {
         <Nav.Link href="/redStops">MBTA Red Line Stops</Nav.Link>
         <Nav.Link href="/orangeStops">MBTA Orange Line Stops</Nav.Link>
         <Nav.Link href="/blueStops">MBTA Blue Line Stops</Nav.Link>
+        <Nav.Link href="/ferrySchedule">MBTA Ferry Schedule</Nav.Link>
+
 </Nav>
     </Container>
   </ReactNavbar>
