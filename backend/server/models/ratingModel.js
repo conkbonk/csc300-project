ea3d7a84e239b592d1c1a6f61601d88b3b50463d
Sam:) <s0372173@salemstate.edu>
2023-04-04 09:11:50 -0400

    rating model

---
+++ backend/server/models/ratingModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+//user schema/model
+const ratingSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      label: "username",
+    },
+    rating: {
+      type: Number,
+      required: true,
+      label: "Rating",
+    }
+  },
+  { collection: "rating" } 
+  //hey
+);
+
+module.exports = mongoose.model('ratings', newUserSchema)
